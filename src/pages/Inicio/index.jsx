d1401f03dc28b952101b7453c4e62b6879e529a5
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-03-11 14:59:25 -0300

    cria rotas e define o absolute path config for vite

---
+++ src/pages/Inicio/index.jsx
@@ -0,0 +1,5 @@
+function Inicio() {
+  return <h1>Olá mundo Inicio!</h1>;
+}
+
+export default Inicio;
---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+import Inicio from 'pages/Inicio';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default AppRoutes;
