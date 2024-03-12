0b6d07ca2d0f964ddd62947030d9b2171da9f9c2
Matheus Rodacki <matheus.rodacki@outlook.com>
2024-03-12 06:29:02 -0300

    estiliza a página Favoritos

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,7 +1,29 @@
+import Banner from 'components/Banner';
 import styles from './Favoritos.module.css';
+import Titulo from 'components/Titulo';
+import Card from 'components/Card';
 
 function Favoritos() {
-  return <></>;
+  return (
+    <>
+      <>
+        <Banner imagem={'favorito'} />
+        <Titulo>
+          <h1>Meus Favoritos</h1>
+        </Titulo>
+
+        <section className={styles.container}>
+          <Card
+            titulo='O labirinto do Logcat'
+            capa='https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img2.png'
+            link='https://www.youtube.com/embed/ypvGqZGJBls'
+            key='1'
+          />
+          ;
+        </section>
+      </>
+    </>
+  );
 }
 
 export default Favoritos;
